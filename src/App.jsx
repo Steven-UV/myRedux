cc73dfccf9ee6f9d93a0babe087af9844cf4bd58
Steven-UV <you@example.com>
2022-03-16 17:18:47 +0800

    reducer 规范 state 的创建流程

--- src/App.jsx
+++ src/App.jsx
@@ -21,15 +21,25 @@ const User = () => {
   const contextValue = useContext(appContext)
   return <div>User:{contextValue.appState.user.name}</div>
 }
+const reducer = (state, {type, payload}) => {
+  if (type === 'updateUser') {
+    return {
+      ...state,
+      user: {
+        ...state.user,
+        ...payload
+      }
+    }
+  }
+}
 const UserModifier = () => {
   const {appState, setAppState} = useContext(appContext)
   const onChange = (e) => {
-    appState.user.name = e.target.value
-    setAppState({...appState})
+    setAppState(reducer(appState, {type: 'updateUser', payload: {name: e.target.value}}))
   }
   return <div>
     <input value={appState.user.name}
-      onChange={onChange}/>
+           onChange={onChange}/>
   </div>
 }
 
